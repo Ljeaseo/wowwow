4288f5c4bcac463cb0956b855fbe9ba10de35f3b
coc_o_pus <ljseo0111@naver.com>
2022-07-08 17:18:57 +0900

    study0001

--- html/WebContent/JAVA/JAVA_study/src/StudyEx01/Study_exTest_01.java
+++ html/WebContent/JAVA/JAVA_study/src/StudyEx01/Study_exTest_01.java
@@ -5,23 +5,17 @@ public class Study_exTest_01 {
         Study_ex01 Study = new Study_ex01();
         Scanner sc = new Scanner(System.in);
         
-
+        int[] array ={Study.major};
         Study.major = sc.nextInt();
-        for(int i = 0; i <= Study.major;i++){
-           if(i==0){
-            Study.Class = sc.nextLine();
+        for(int i = 1; i <= Study.major;i++){
+  
+            Study.Class = sc.next();
             Study.grade = sc.nextInt();
             Study.stno = sc.nextInt();
-           }else if(i==1){
-            Study.Class = sc.nextLine();
-            Study.grade = sc.nextInt();
-            Study.stno = sc.nextInt();
-           }
-            
         }
-         System.out.println(Study.Class);
-         System.out.println(Study.grade);
-         System.out.println(Study.stno);
+         System.out.println(array[0]);
+         System.out.println(array[1]);
+         System.out.println(array[2]);
         
  
     }
